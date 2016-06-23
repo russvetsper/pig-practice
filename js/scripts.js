73fc334670e5da31f4ea25b709186a1366e7c7e8
RussRouz <RussRouz@RouzRuss>
2016-06-23 14:41:14 -0700

    first 2 functions done

---
+++ js/scripts.js
@@ -0,0 +1,25 @@
+function vowelTester(input) {
+  var vowels=["a","e","i","o","u"];
+  var result = vowels.indexOf(input);
+  if (result > -1) {
+    return true
+  } else {
+    return false;
+  }
+};
+
+function wordTranslator(input) {
+  var firstLetter = input[0];
+  var result = vowelTester(firstLetter);
+  var output = "";
+  if (result) {
+    output = input.concat("ay");
+  } else {
+    output = input.slice(1) + input[0].concat("ay");
+  }
+  return output;
+};
+
+function sentenceTranslator() {
+
+}
